eb0e9cd096054b29dfca34583b02991d411f7b4c
guilhermefc <guilherme.f.carneiro@gmail.com>
2022-09-09 10:14:04 -0300

    deploy

--- main.go
+++ main.go
@@ -34,6 +34,9 @@ func main() {
 		log.Fatal(err)
 	}
 }
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
 
 type Searcher struct {
 	CompleteWorks string
@@ -42,6 +45,7 @@ type Searcher struct {
 
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
 		query, ok := r.URL.Query()["q"]
 		if !ok || len(query[0]) < 1 {
 			w.WriteHeader(http.StatusBadRequest)
