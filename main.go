5507060c46d0e896bfdff391eec7dc532f66df4b
guilhermefc <guilherme.f.carneiro@gmail.com>
2022-09-14 23:24:41 -0300

    add pagination on server

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -44,10 +45,24 @@ type Searcher struct {
 	SuffixArray   *suffixarray.Index
 }
 
+type Response struct {
+	CurrentPage      int
+	TotalPagesLength int
+	TotalItemsLength int
+	Items            []string
+}
+
 func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		enableCors(&w)
 		query, ok := r.URL.Query()["q"]
+
+		page := 1
+		if len(r.URL.Query()["page"]) == 1 {
+			var _ error
+			page, _ = strconv.Atoi(r.URL.Query()["page"][0])
+		}
+
 		if !ok || len(query[0]) < 1 {
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("missing search query in URL params"))
@@ -56,7 +71,25 @@ func handleSearch(searcher Searcher) func(w http.ResponseWriter, r *http.Request
 		results := searcher.Search(query[0])
 		buf := &bytes.Buffer{}
 		enc := json.NewEncoder(buf)
-		err := enc.Encode(results)
+
+		var maxLength = 20 * page
+		var minLenght = maxLength - 20
+		totalItemsLength := len(results)
+		totalPagesLength := len(results) / 20
+
+		if len(results) > maxLength {
+			results = results[minLenght:maxLength]
+		}
+
+		response := Response{
+			CurrentPage:      page,
+			TotalPagesLength: totalPagesLength,
+			TotalItemsLength: totalItemsLength,
+			Items:            results,
+		}
+
+		err := enc.Encode(response)
+
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte("encoding failure"))
